63460e1d810f1b979f06e7715339711aa4fd71b9
adde9708 <adde.edvin@gmail.com>
2021-09-10 13:51:05 +0200

    remove unsused logger

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,17 +1,5 @@
 import discord
-import logging
 
-
-"""def create_logger():
-    logger = logging.getLogger('discord')
-    logger.setLevel(logging.INFO)
-    handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
-    handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
-    logger.addHandler(handler)
-
-
-create_logger()
-"""
 client = discord.Client()
 
 

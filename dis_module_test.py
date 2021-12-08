8aa80f8ed6c3ff3de3909e52b587c615ed6a2110
adde9708 <adde.edvin@gmail.com>
2021-12-08 14:48:12 +0100

    add a few more scripts and files that got incorrectly ignored

---
+++ dis_module_test.py
@@ -0,0 +1,22 @@
+import dis
+from time import sleep
+done = False
+
+
+def if_statement():
+    done = False
+
+    while not done:
+        done = True
+        if done:
+            print()
+            print("done")
+            print()
+            sleep(2)
+        else:
+            return print("dead code")
+
+
+if_statement()
+dis.dis(if_statement)
+sleep(22)

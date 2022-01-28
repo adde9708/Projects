794eac359aba058f40687eb4892af10d61678b2a
adde9708 <adde.edvin@gmail.com>
2022-01-28 14:47:11 +0100

    add some tests to zip.py

--- zip.py
+++ zip.py
@@ -1,13 +1,22 @@
 import time
+import pytest
 
 a = ["Michael, William, Harper"]
 b = ["John, Evelyn, Ryan"]
 
+c = a + b
 
-def test_zip():
+
+def trying_zip():
     print(list(zip(a, b)))
 
 
-test_zip()
+trying_zip()
 
+
+def test_zip():
+    assert a + b == c
+
+
+test_zip()
 print(time.process_time())

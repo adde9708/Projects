f8dc43c2d102858f7588eb63ab5628cfd1240e1d
adde9708 <adde.edvin@gmail.com>
2022-02-16 14:00:40 +0100

    git didn't add the python file  for some reason

---
+++ potatos.py
@@ -0,0 +1,18 @@
+
+
+def potatos():
+    bowl = []
+    potato = "potato"
+    full = False
+
+    while not full:
+        bowl.append(potato)
+        if len(bowl) == 500:
+            print(tuple(bowl))
+            full = True
+    return full
+
+
+potatos()
+
+

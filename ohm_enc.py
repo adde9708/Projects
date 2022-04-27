9d63668a35a13ec6e8345e86109fe5ebd02f082c
adde9708 <adde.edvin@gmail.com>
2022-04-27 14:37:56 +0200

    updated a script

--- ohm_enc.py
+++ ohm_enc.py
@@ -1,6 +1,7 @@
 import secrets
 import math
 import hashlib
+import gc
 
 
 def ohm_enc():
@@ -14,21 +15,35 @@ def ohm_enc():
     key3 = E / i
 
     list_of_equations = (key, key2, key3)
-
-    for _ in list_of_equations:
-        keys.append(list_of_equations)
-
+    keys.append(list_of_equations)
+    x = tuple(keys)
+    for list_of_equations in x:
         key = secrets.choice(keys)
-    for key in list_of_equations:
+
+        key2 = secrets.choice(keys)
+        key2 = str(key2).replace('(', "").replace(')', "").replace(' ', "").replace(
+            'e', "").replace('-', "").replace('.', "").replace(',', "")
+        key2 = float(key2)
+        key2 = math.ceil(key2)
+        key2 = hex(key2).rstrip("0")
+        key2 = hashlib.shake_256(bytes(key2, encoding='utf-8'))
+        print(key2.hexdigest(256))
+        key3 = secrets.choice(keys)
+        key3 = str(key3).replace('(', "").replace(')', "").replace(' ', "").replace(
+            'e', "").replace('-', "").replace('.', "").replace(',', "")
+        key3 = float(key3)
+        key3 = math.ceil(key3)
+        key3 = hex(key3).rstrip("0")
+        key3 = hashlib.shake_256(bytes(key3, encoding='utf-8'))
+        print(key3.hexdigest(256))
         key = str(key).replace('(', "").replace(')', "").replace(' ', "").replace(
             'e', "").replace('-', "").replace('.', "").replace(',', "")
         key = float(key)
-
         key = math.ceil(key)
-
-        key = hex(key)
+        key = hex(key).rstrip("0")
         key = hashlib.shake_256(bytes(key, encoding='utf-8'))
         print(key.hexdigest(256))
 
 
 ohm_enc()
+

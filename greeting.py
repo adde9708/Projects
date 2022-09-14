bf3774340896e1b80f75ab51a829844b89419009
adde9708 <adde.edvin@gmail.com>
2022-09-14 14:26:31 +0200

    Added a new script

---
+++ greeting.py
@@ -0,0 +1,12 @@
+def greeting():
+  we_want_to_mess_with_greeting = False
+
+  if we_want_to_mess_with_greeting == False:
+    greeting = "Hej och Tack! Jag blev inte borttagen!"
+    print(greeting)
+  else:
+    greeting = "Åh nej! Jag blev ändrad!"
+    print(greeting)
+greeting()
+
+    

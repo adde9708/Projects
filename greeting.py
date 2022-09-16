7e9fcbcdd46e2c7bfdf09bfbd91240c2111797b7
adde9708 <adde.edvin@gmail.com>
2022-09-16 13:24:23 +0200

    changed a script to english

--- greeting.py
+++ greeting.py
@@ -2,11 +2,12 @@ def greeting():
   we_want_to_mess_with_greeting = False
 
   if we_want_to_mess_with_greeting == False:
-    greeting = "Hej och Tack! Jag blev inte borttagen!"
+    greeting = "Hey and thank you! I didn't get removed!"
     print(greeting)
   else:
-    greeting = "Åh nej! Jag blev ändrad!"
+    greeting = "Oh no! I got changed!"
     print(greeting)
+
 greeting()
 
-    
+

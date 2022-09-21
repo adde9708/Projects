16fde29a88d4f9061c76e3e126b28ed7dba1ba0e
adde9708 <adde.edvin@gmail.com>
2022-09-21 14:29:51 +0200

    changed the placing of the print

--- time_test.py
+++ time_test.py
@@ -1,17 +1,18 @@
 import time
 
 def for_loop():
-    print("for loop")
+    
     for i in range(1, 101):
         print(i)
-
+    print("for loop")
+    
 def while_loop():
-    print("while loop")
+  
     x = 0
     while x < 100:
         x+=1
         print(x)
-
+    print("while loop")
 for_loop()
 print(time.process_time())
 while_loop()

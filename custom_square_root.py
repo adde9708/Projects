917010de410800a0a70af1d690ee1c767327d84f
adde9708 <adde.edvin@gmail.com>
2022-12-10 22:08:36 +0100

    added new script

---
+++ custom_square_root.py
@@ -0,0 +1,8 @@
+def custom_square_root(x) -> float:
+    xhalf = 0.5 * x
+    xhalf = int(xhalf)
+    i = x = xhalf & int(x)
+    i = 0x5f3759df - (i >> 1)
+    x = float(i)
+    x = x * (1.5 - xhalf*x*x)
+    return print(x)

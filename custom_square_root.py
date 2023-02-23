54f365e6d6722e6a55dcc61863e7c5af2be37255
adde9708 <adde.edvin@gmail.com>
2023-02-23 18:37:44 +0100

    fixed return type of the square root script

--- custom_square_root.py
+++ custom_square_root.py
@@ -1,11 +1,12 @@
-def custom_square_root(x) -> float:
+def custom_square_root(x: float) -> float:
     xhalf = 0.5 * x
     xhalf = int(xhalf)
     i = x = xhalf & int(x)
     i = 0x5f3759df - (i >> 1)
     x = float(i)
     x = x * (1.5 - xhalf * x * x)
-    return print(x)
+    print(x)
+    return x
 
 
 custom_square_root(3000000)

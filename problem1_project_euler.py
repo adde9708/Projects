dc56a5d99ab90fda7faad3bfecee34f15d7baaf0
adde9708 <adde.edvin@gmail.com>
2023-02-24 13:44:04 +0100

    formatted a script

--- problem1_project_euler.py
+++ problem1_project_euler.py
@@ -1,11 +1,12 @@
 from typing import TypeVar
 
-T = TypeVar('T', int, int )
+T = TypeVar('T', int, int)
+
 
 def multiples_of_3_and_5() -> list[T]:
- 
+
     results = []
-    
+
     for numbers in range(1, 1000):
 
         if numbers % 3 == 0 or numbers % 5 == 0:
@@ -14,5 +15,6 @@ def multiples_of_3_and_5() -> list[T]:
             print(sum(results))
 
     return results
+
 
 multiples_of_3_and_5()

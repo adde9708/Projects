9accf7e094045c72270bec2f8842771cca8a8c84
adde9708 <adde.edvin@gmail.com>
2023-02-24 13:36:21 +0100

    added typing to an old script

--- problem1_project_euler.py
+++ problem1_project_euler.py
@@ -1,14 +1,18 @@
+from typing import TypeVar
 
+T = TypeVar('T', int, int )
 
-def multiples_of_3_and_5():
+def multiples_of_3_and_5() -> list[T]:
+ 
     results = []
+    
     for numbers in range(1, 1000):
 
         if numbers % 3 == 0 or numbers % 5 == 0:
+
             results.append(numbers)
             print(sum(results))
 
+    return results
 
 multiples_of_3_and_5()
-
-

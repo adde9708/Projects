e34ca0101e554be03451a830ab598678c7572b0d
adde9708 <adde.edvin@gmail.com>
2023-02-24 14:46:14 +0100

    small change in a script

--- problem1_project_euler.py
+++ problem1_project_euler.py
@@ -2,7 +2,8 @@ def multiples_of_3_and_5(n: int) -> int:
     numbers = set(range(3, n, 3)) | set(range(5, n, 5))
 
     total_sum = sum(numbers)
-    return print(total_sum)
+    print(total_sum)
+    return total_sum
 
 
 multiples_of_3_and_5(1000)

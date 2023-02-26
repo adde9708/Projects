58c3cca1f410531ccb0544a9df5df398cdcdb2af
adde9708 <adde.edvin@gmail.com>
2023-02-26 20:16:38 +0100

    added type hints to arguments in the function

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,8 +1,8 @@
 
-lst = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+lst: tuple[int, ...] = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 
-def sequential_search(lst_, num: int) -> bool:
+def sequential_search(lst_: tuple[int, ...], num: int) -> bool:
     for i in lst_:
 
         if i == num:

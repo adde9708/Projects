301ba800d429a4db82495f7484bc6b727716a0a7
adde9708 <adde.edvin@gmail.com>
2023-02-26 20:22:09 +0100

    add typing to greeting.py

--- greeting.py
+++ greeting.py
@@ -1,13 +1,14 @@
-def greeting():
-  we_want_to_mess_with_greeting = False
+def greeting() -> str:
+    we_want_to_mess_with_greeting = False
 
-  if we_want_to_mess_with_greeting == False:
-    greeting = "Hey and thank you! I didn't get removed!"
-    print(greeting)
-  else:
-    greeting = "Oh no! I got changed!"
-    print(greeting)
+    if we_want_to_mess_with_greeting == False:
+        greeting = "Hey and thank you! I didn't get removed!"
+        print(greeting)
+        return greeting
+    else:
+        greeting = "Oh no! I got changed!"
+        print(greeting)
+        return greeting
+
 
 greeting()
-
-

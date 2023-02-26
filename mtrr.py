8ac4921028bca5547f3dd36b7195a073cd977f22
adde9708 <adde.edvin@gmail.com>
2023-02-26 22:05:13 +0100

    fixed the mttr script

--- mtrr.py
+++ mtrr.py
@@ -1,24 +1,16 @@
-def mtrr_get(n: int) -> int:
-    phys_mask: int = 0x201 + (n * 2)
-    phys_base: int = 0x200 + (n * 2)
-    mask_base: int = phys_mask + phys_base
-    addr = hex(0x80000000)
-    target_addr = addr + str(mask_base)
-    target_addr = target_addr.lstrip("0").lstrip("x").strip()
-    target_addr = float(target_addr)
-    target_addr = str(target_addr)
-    target_addr = target_addr.rstrip("0").rstrip(".").strip()
-    mask_target: int = mask_base + int(target_addr)
-    mask_base = mask_target
+def mtrr_get(n: int) -> bool:
+    phys_mask: int = 0x201 + (n * 2)  # physical mask address
+    phys_base: int = 0x200 + (n * 2)   # physical base address
+    mask_base: int = phys_base & phys_mask  # mask for base address
+    addr = 0x80000000
+    target_addr: int = addr & mask_base
 
-    if mask_base == mask_target:
-        target_addr = int(target_addr)
+    if mask_base == target_addr:
         print(target_addr)
-        return target_addr in range(mask_base, mask_target)
+        return target_addr in range(mask_base, mask_base + (2 ** 12))
     else:
-        target_addr = int(target_addr)
         print(target_addr)
-        return target_addr not in range(mask_base, mask_target)
+        return target_addr not in range(mask_base, mask_base + (2 ** 12))
 
 
 mtrr_get(333)

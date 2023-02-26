59a3e8419d1da87c84fc0b3e200143bd21afcc34
adde9708 <adde.edvin@gmail.com>
2023-02-26 20:12:38 +0100

    fix a missed typo

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,7 +1,8 @@
+
 lst = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
 
 
-def sequential__search(lst_, num):
+def sequential_search(lst_, num: int) -> bool:
     for i in lst_:
 
         if i == num:
@@ -9,7 +10,8 @@ def sequential__search(lst_, num):
 
             print(i)
 
-            return print()
+            print()
+            return True
     print()
 
     print(f"{num} does not exist in the given list")
@@ -19,4 +21,4 @@ def sequential__search(lst_, num):
     return False
 
 
-sequential__search(lst, 10)
+sequential_search(lst, 10)

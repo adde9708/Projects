9974309ddac61c4b31cc013a32144e2f6e32d355
adde9708 <adde.edvin@gmail.com>
2023-02-26 20:01:18 +0100

    optimized the cell_test a bit

--- cell_test.py
+++ cell_test.py
@@ -1,24 +1,15 @@
 from operator import mul
-from typing import TypeVar
+from typing import Tuple
 
 
-T = TypeVar('T', float, float)
+def vmult(arr: Tuple[float, float, float, float, float, float, float, float],
+          arr2: Tuple[float, float, float, float, float, float, float, float],
+          size: int) -> Tuple[float, float, float, float, float, float,
+                              float, float]:
 
-
-def vmult(arr: tuple[T, T, T, T, T, T, T, T],
-          arr2: tuple[T, T, T, T, T, T, T, T], size: int) -> int:
-
-    array_size_by_four = size >> 2
-    varr = arr[0:7] * array_size_by_four
+    array_size_by_four = size // 4
+    varr = arr[0:8] * array_size_by_four
     varr2 = arr2[0:8] * array_size_by_four
-
-    for _ in range(array_size_by_four):
-        vout = tuple(map(mul, varr, varr2))
-        print(vout)
-        return 0
-
-    return 0
-
-
-vmult((2.2, 4.4, 8.2, 16.4, 32.4, 64.4, 128.4, 256.4),
-      (2.4, 4.8, 8.8, 16.8, 32.8, 64.8, 128.8, 256.8), 8)
+    vout = tuple(map(mul, varr, varr2))
+    print(vout)
+    return vout

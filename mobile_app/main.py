2f1accf20e9ec8d986ebf43b664cbf113d09465d
adde9708 <adde.edvin@gmail.com>
2023-03-02 01:46:51 +0100

    removed unused module

--- mobile_app/main.py
+++ mobile_app/main.py
@@ -2,7 +2,6 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
-from kivy.graphics import BorderImage
 import secrets
 from pathlib import Path
 

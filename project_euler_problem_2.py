4292a4405fa7acca5a88fd56b159e7abc8c2833f
adde9708 <adde.edvin@gmail.com>
2023-03-29 14:03:21 +0200

    once again commited the wrong version

--- project_euler_problem_2.py
+++ project_euler_problem_2.py
@@ -1,6 +1,19 @@
 def sum_of_even_numbers_fibonacci(num: int):
-    fibonacci = {1, 2}
-    while max(fibonacci) + min(fibonacci) <= num:
-        fibonacci.add(max(fibonacci) + min(fibonacci))
-    even_numbers = {x for x in fibonacci if x % 2 == 0}
-    print(sum(even_numbers))
+    if num < 2:
+        print(0)
+        return
+    
+    # initialize variables
+    prev = 2
+    curr = 8
+    total = 2  # start with the sum of the first even Fibonacci number
+    
+    # loop through even Fibonacci numbers up to num
+    while curr <= num:
+        total += curr
+        prev, curr = curr, 4 * curr + prev
+    
+    print(total)
+
+    
+sum_of_even_numbers_fibonacci(4000900)

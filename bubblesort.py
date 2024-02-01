da6552033b76e10326896c58bd7b5e5848e0b7c7
adde9708 <adde.edvin@gmail.com>
2024-02-01 14:42:52 +0100

    made another version of bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -1,3 +1,4 @@
+
 def bubble_sort(speed):
     n = len(speed)
     swapped = True
@@ -12,6 +13,36 @@ def bubble_sort(speed):
 
 speed = [100, 80, 130, 111, 96, 110, 90, 94, 86, 150, 120, 144, 146]
 bubble_sort(speed)
+
+
+for item in range(len(speed)):
+    print(speed[item])
+
+
+print()
+
+
+speed = [100, 80, 130, 111, 96, 110, 90, 94, 86, 150, 120, 144, 146]
+
+
+def bubble_sort_2(speed: list):
+    if len(speed) <= 1:
+        return speed
+
+    if len(speed) == 2:
+        return speed if speed[0] < speed[1] else [speed[1], speed[0]]
+
+    first_elem, second_elem, sub_array = speed[0], speed[1], speed[2:]
+    result = []
+    if first_elem < second_elem:
+        result = [first_elem] + bubble_sort_2([first_elem] + sub_array)
+    else:
+        result = [second_elem] + bubble_sort_2([first_elem] + sub_array)
+
+    return bubble_sort_2(result[:-1]) + result[-1:]
+
+
+bubble_sort_2(speed)
 
 for item in range(len(speed)):
     print(speed[item])

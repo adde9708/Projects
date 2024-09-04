d4ff6c324c918a4215f478a0defb5342c0a142e0
adde9708 <adde.edvin@gmail.com>
2024-09-04 15:05:00 +0200

    Removed comments

--- hangman.py
+++ hangman.py
@@ -1,6 +1,3 @@
-# Första versionen av vårt spel kommer vara simplistiskt men fortfarande ett
-# fungerande program. Under kommande veckor kommer vi lägga till fler funktioner
-# samt dela upp programmet i flera filer.
 import random
 
 POSSIBLE_WORDS = (
